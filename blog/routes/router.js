3fbb47238b04523ddf89ac0d81359c77c77c7b48
kokole12 <kokoleismail886@gmail.com>
2023-12-11 12:50:21 +0300

    image upload

--- blog/routes/router.js
+++ blog/routes/router.js
@@ -3,6 +3,24 @@ import AuthController from '../controllers/AuthController.js'
 import { loginValidation, validate, registerValidation, postCreateValidation } from '../utils/validation.js'
 import PostController from '../controllers/postController.js'
 import authenticate from '../middlewares/authMiddleware.js'
+import multer from 'multer'
+import fs from 'fs'
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadDir = './uploads'
+    if (!fs.existsSync(uploadDir)) {
+      fs.mkdirSync(uploadDir)
+    }
+    cb(null, uploadDir)
+  },
+  filename: (req, file, cb) => {
+    const filename = `${Date.now()}_${file.originalname}`
+    cb(null, filename)
+  }
+})
+
+const upload = multer({ storage })
 
 const router = express.Router()
 
@@ -14,7 +32,7 @@ router.get('/api/v1/verify/:token', AuthController.verifyAccount)
 
 router.get('/api/v1/blog/posts', authenticate, PostController.getPosts)
 
-router.post('/api/v1/blog/posts', authenticate, postCreateValidation, validate, PostController.createPost)
+router.post('/api/v1/blog/posts', authenticate, upload.single('image'), postCreateValidation, validate, PostController.createPost)
 
 router.delete('/api/v1/blog/posts/:id', authenticate, PostController.deletePost)
 
