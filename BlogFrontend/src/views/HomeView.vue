bc432f137408b74484d526c944ac337d02026985
kokole12 <kokoleismail886@gmail.com>
2023-12-12 14:32:44 +0300

    frontend setup

---
+++ BlogFrontend/src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="flex items-center justify-center bg-black">
+        Home view
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
