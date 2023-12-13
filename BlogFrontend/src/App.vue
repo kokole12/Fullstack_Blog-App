6e705892df37265b8874b7641d9fb8ff256d3b84
kokole12 <kokoleismail886@gmail.com>
2023-12-13 10:56:54 +0300

    navbar

--- BlogFrontend/src/App.vue
+++ BlogFrontend/src/App.vue
@@ -1,8 +1,10 @@
 <template>
+  <NavBar/>
   <main>Main component</main>
   <router-view></router-view>
 </template>
 
 <script setup>
+import NavBar from './components/NavBar.vue';
 
 </script>
---
+++ BlogFrontend/src/components/NavBar.vue
@@ -0,0 +1,33 @@
+<template>
+    <header class="flex items-center justify-around h-full p-3 bg-slate-400">
+        <div>
+            <a href="" class="logo">My Blog</a>
+            <input type="text" class="p-2 mx-3 border-solid border-black rounded-3xl" placeholder="search">
+        </div>
+        <div>
+            <ul class="flex items-center justify-around">
+                <li>
+                    <a href="" class="mx-2">Home</a>
+                </li>
+                <li>
+                    <a href="" class="mx-2">Stories</a>
+                </li><li>
+                    <a href="" class="mx-2">My stories</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            <a href="" class="mx-2">Write</a>
+            <a href="" class="mx-2">login</a>
+            <a href="" class="mx-2">Sign up</a>
+        </div>
+    </header>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
